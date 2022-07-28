2051a470a19d0ff808c078754697b184be5de0e3
Xin Yong <xiyon@microsoft.com>
2022-07-28 23:12:16 +0800

    fix the profiling job for new interface

--- code/profiling/how-to-profile-online-endpoint.sh
+++ code/profiling/how-to-profile-online-endpoint.sh
@@ -9,6 +9,7 @@
 ## 5. az login
 ## 6. az account set --subscription "<YOUR_SUBSCRIPTION>"
 ## 7. az configure --defaults group=<RESOURCE_GROUP> workspace=<WORKSPACE_NAME>
+set -x
 
 # <set_variables>
 export ENDPOINT_NAME="${ENDPOINT_NAME}"
@@ -16,38 +17,33 @@ export DEPLOYMENT_NAME="${DEPLOYMENT_NAME}"
 export SKU_CONNECTION_PAIR=${SKU_CONNECTION_PAIR}
 export PROFILING_TOOL=wrk # allowed values: wrk, wrk2 and labench
 export PROFILER_COMPUTE_NAME="${PROFILER_COMPUTE_NAME}" # the compute name for hosting the profiler
-export DURATION="" # time for running the profiling tool (duration for each wrk call or labench call), default value is 300s
 export CONNECTIONS=`echo $SKU_CONNECTION_PAIR | awk -F: '{print $2}'` # for wrk and wrk2 only, no. of connections for the profiling tool, default value is set to be the same as the no. of workers, or 1 if no. of workers is not set
-export THREAD="" # for wrk and wrk2 only, no. of threads allocated for the profiling tool, default value is 1
-export TARGET_RPS="" # for labench and wrk2 only, target rps for the profiling tool, default value is 50
-export CLIENTS="" # for labench only, no. of clients for the profiling tool, default value is set to be the same as the no. of workers, or 1 if no. of workers is not set
-export TIMEOUT="" # for labench only, timeout for each request, default value is 10s
 # </set_variables>
-
-# <upload_payload_file_to_default_blob_datastore>
-default_datastore_info=`az ml datastore show --name workspaceblobstore -o json`
-account_name=`echo $default_datastore_info | jq '.account_name' | sed "s/\"//g"`
-container_name=`echo $default_datastore_info | jq '.container_name' | sed "s/\"//g"`
-connection_string=`az storage account show-connection-string --name $account_name -o tsv`
-az storage blob upload --container-name $container_name/profiling_payloads --name ${ENDPOINT_NAME}_payload.txt --file profiling/payload.txt --connection-string $connection_string
-# </upload_payload_file_to_default_blob_datastore>
 
 # <create_profiling_job_yaml_file>
 # please specify environment variable "IDENTITY_ACCESS_TOKEN" when working with ml compute with no appropriate MSI attached
 sed \
   -e "s/<% ENDPOINT_NAME %>/$ENDPOINT_NAME/g" \
-  -e "s/<% DEPLOYMENT_NAME %>/$DEPLOYMENT_NAME/g" \
-  -e "s/<% PROFILING_TOOL %>/$PROFILING_TOOL/g" \
-  -e "s/<% DURATION %>/$DURATION/g" \
-  -e "s/<% CONNECTIONS %>/$CONNECTIONS/g" \
-  -e "s/<% TARGET_RPS %>/$TARGET_RPS/g" \
-  -e "s/<% CLIENTS %>/$CLIENTS/g" \
-  -e "s/<% TIMEOUT %>/$TIMEOUT/g" \
-  -e "s/<% THREAD %>/$THREAD/g" \
   -e "s/<% COMPUTE_NAME %>/$PROFILER_COMPUTE_NAME/g" \
   -e "s/<% SKU_CONNECTION_PAIR %>/$SKU_CONNECTION_PAIR/g" \
   profiling/profiling_job_tmpl.yml > ${ENDPOINT_NAME}_profiling_job.yml
+
+sed \
+  -e "s/<% ENDPOINT_NAME %>/$ENDPOINT_NAME/g" \
+  -e "s/<% DEPLOYMENT_NAME %>/$DEPLOYMENT_NAME/g" \
+  -e "s/<% PROFILING_TOOL %>/$PROFILING_TOOL/g" \
+  -e "s/<% CONNECTIONS %>/$CONNECTIONS/g" \
+  profiling/config_tmp.json > profiling/config.json
 # </create_profiling_job_yaml_file>
+
+# <upload_payload_file_and_config_file_to_default_blob_datastore>
+default_datastore_info=`az ml datastore show --name workspaceblobstore -o json`
+account_name=`echo $default_datastore_info | jq '.account_name' | sed "s/\"//g"`
+container_name=`echo $default_datastore_info | jq '.container_name' | sed "s/\"//g"`
+connection_string=`az storage account show-connection-string --name $account_name -o tsv`
+az storage blob upload --container-name $container_name/profiling_payloads --name ${ENDPOINT_NAME}_payload.txt --file profiling/payload.txt --connection-string $connection_string
+az storage blob upload --container-name $container_name/profiling_configs --name ${ENDPOINT_NAME}_config.json --file profiling/config.json --connection-string $connection_string
+# </upload_payload_file_and_config_file_to_default_blob_datastore>
 
 # <create_profiling_job>
 run_id=$(az ml job create -f ${ENDPOINT_NAME}_profiling_job.yml --query name -o tsv)
